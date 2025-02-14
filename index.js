5486c02fafa5d105deccd4ff0f00f3ec4ff8c28c
Navinofc44 <Pahasarasanujja@gmail.com>
2025-02-15 00:25:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,27 +80,38 @@ console.log('Queen-Nethu-MD connected ✅')
     })
 
   conn.ev.on('creds.update', saveCreds)
-  
-//------- *STATUS AUTO REACT* ----------
-
-conn.ev.on('messages.upsert', async(mek) => {
+  conn.ev.on('messages.upsert', async (mek) => {
+    try {
 mek = mek.messages[0]
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-      await conn.readMessages([mek.key])
-    }
-  if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-    const emojis = ['🥇', '😹', '💜', '😂', '🎊', '🌝', '😒', '🍂', '🌟', '🎋', '😏', '🎀', '🌸', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'];
-    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-    await conn.sendMessage(mek.key.remoteJid, {
-      react: {
-        text: randomEmoji,
-        key: mek.key,
-      } 
-    }, { statusJidList: [mek.key.participant] });
-  }
+if (!mek.message) return
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 
-
-//----```© Pink Venom ofc```-----------🗿🤌
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_READ === "true") {
+            try {
+                await conn.readMessages([mek.key]);
+                const mnyako = await jidNormalizedUser(conn.user.id);
+                if (!mek.key.id) {
+                    console.error("Invalid message key: missing 'id'");
+                    return;
+                }
+        
+                await conn.sendMessage(
+                    mek.key.remoteJid,
+                    {
+                        react: {
+                            key: { remoteJid: mek.key.remoteJid, id: mek.key.id },
+                            text: config.AUTO_SREACT_KEY || '💚', 
+                        }
+                    },
+                    {
+                        statusJidList: [mek.key.participant || mek.key.remoteJid, mnyako] 
+                    }
+                );
+            } catch (error) {
+                console.error("Error in auto status react:", error);
+            }
+        }
       
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
