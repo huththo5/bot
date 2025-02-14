5bddb0d5fc7a5e79fe692958d02f05826f5f7a8b
Navinofc44 <Pahasarasanujja@gmail.com>
2025-02-14 23:15:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'WIgnjBZK#FWHW-uMzwd0TxiIRRGwtYGA0P7ImJ8msQVhONrSZTJE',
+SESSION_ID: 'FSdQWC6D#q4AMBwaNsRIF9ZbZCBuwN1N0Rzzm5n3kvGrJKGBc2Q4',
 GITHUB_AUTH_TOKEN: 'FKCftYEgo3ZhDMN5dz09fdHthrdFvx05CV0P', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
 GITHUB_USER_NAME: 'deploy585',
 AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "true",
